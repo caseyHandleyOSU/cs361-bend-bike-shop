20840dff8e838eeff3cd98ef2b9c31c508843eb1
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-11-16 17:33:50 -0800

    Comment Rentable.rb

--- rentable.rb
+++ rentable.rb
@@ -5,9 +5,10 @@ class Rentable
   def initialize(isRented, cost, duration)
     @isRented = isRented
     @cost = cost
-    @duration = duration
+    @duration = duration # Example of other data that this class could have
   end
 
+  # Mark the Rentable as rented
   def rent!
     @isRented = true
   end
