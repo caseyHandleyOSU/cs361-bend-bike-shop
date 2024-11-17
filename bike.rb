b3163da98af0e0eb0c055623075b3e8001b8bc77
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-11-16 17:32:57 -0800

    pass rental price into bike's rentalData obj

--- bike.rb
+++ bike.rb
@@ -13,10 +13,9 @@ class Bike
     # Necessary Bike Information that probably shouldn't be abstracted!
     @id = id
     @color = color
-    @price = price
     @weightLBS = weight
     # Not exclusive to a bike
-    @rentalData = Rentable.new(rented, 0, 0) # Rentable class is used to extract data that may not be exclusive to bike rentals, without needing to change the [bikeInstance].rent! signature
+    @rentalData = Rentable.new(rented, price, 0) # Rentable class is used to extract data that may not be exclusive to bike rentals, without needing to change the [bikeInstance].rent! signature
     @cargo = Inventory.new([], 10) # A bike's cargo is basically just an inventory, which could be used elsewhere (such as storing the catalog of bikes!)
   end
 
